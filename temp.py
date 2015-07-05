f1dd073ee8c1c8c07916d2355a39653f59f35082
mmitti <mmitti.mail@gmail.com>
2015-07-05 18:46:56 +0900

    罫線変更

--- temp.py
+++ temp.py
@@ -4,16 +4,14 @@ import curses
 
 def main(screen):
     frame = curses.initscr()
-    frame.attrset(0)
-    frame.bkgdset(1)
     w = curses.newwin(10, curses.tigetnum("cols") - 2, 0, 1)
     w2 = curses.newwin(6, 40, 2, 2)
     w.addstr(1, 34, "mmitti server")
     w2.addstr(1, 10, "Component A")
 
     w.move(9, 39)
-    w.border()
-    w2.border()
+    w.border('|', '|', '-', '-', '+', '+', '+', '+')
+    w2.border('|', '|', '-', '-', '+', '+', '+', '+')
 
     frame.refresh()
     w.refresh()
