79143e8de1696ca4acdc8430599f9a10012ec3db
mmitti <mmitti.mail@gmail.com>
2015-07-05 18:39:53 +0900

    処理方法修正

--- temp.py
+++ temp.py
@@ -2,19 +2,22 @@
 import os
 import curses
 
-frame = curses.initscr()
-w = curses.newwin(10, curses.tigetnum("cols") - 2, 0, 1)
-dummy_frame = curses.newwin(1, 1, 9, curses.tigetnum("cols"))
-w2 = curses.newwin(6, 40, 2, 2)
-w.addstr(1, 34, "mmitti server")
-w2.addstr(1, 10, "Component A")
+def main(screen):
+    frame = curses.initscr()
+    frame.attrset(0)
+    frame.bkgdset(1)
+    w = curses.newwin(10, curses.tigetnum("cols") - 2, 0, 1)
+    w2 = curses.newwin(6, 40, 2, 2)
+    w.addstr(1, 34, "mmitti server")
+    w2.addstr(1, 10, "Component A")
 
-w.move(9, 39)
-w.border()
-w2.border()
+    w.move(9, 39)
+    w.border()
+    w2.border()
 
-w.refresh()
-w2.refresh()
-dummy_frame.refresh()
-curses.napms(1000)
-curses.endwin()
+    frame.refresh()
+    w.refresh()
+    w2.refresh()
+    curses.napms(1000)
+
+curses.wrapper(main)
