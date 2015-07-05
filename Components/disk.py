f9f811f71a1c1323f6a3d213e82dee31b2fe1249
mmitti <mmitti.mail@gmail.com>
2015-07-05 13:43:39 +0900

    ディスク関係追加

---
+++ Components/disk.py
@@ -0,0 +1,23 @@
+import component
+import os
+import utils
+class DiskUsage(component.Component):
+    def __init__(self):
+        component.Component.__init__(self)
+        return
+    def getUsage(self, path, label):
+        try:
+            s = os.statvfs(path)
+        except OSError:
+            return label + ":N/A"
+        #avail = s.f_bsize * s.f_bavail
+        total = s.f_bsize * s.f_blocks
+        free = s.f_bsize * s.f_bfree
+        used = total - free
+        return label + ":"+str(used*100/total)+"%"
+    _disks = (("/", "ROOT"), ("/mnt/data", "DATA"), ("/mnt/dlna", "DLNA"))
+    def show(self):
+        ret = "FILE SYSTEM: "
+        for v in self._disks:
+            ret += self.getUsage(v[0], v[1]) + " "
+        return ret
--- Components/utils.py
+++ Components/utils.py
@@ -1,4 +1,4 @@
-import shlex, subprocess
+import shlex, subprocess, math
 
 def docmd(cmd):
     cmd_list = shlex.split(cmd)
@@ -8,3 +8,12 @@ def cat(path):
     ret = f.read()
     f.close()
     return ret
+def addSIPrefix(val):
+    prefix = ["", "k", "M", "G", "T", "P"]
+    div = 0
+    for p in prefix:
+        if(val >= 1024**(div+1)):
+            div = div + 1
+            continue
+        return str(round(val / 1024**div, 2)) + p
+    return str(val)
